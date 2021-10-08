6aca6332ace26cd27cba9250c5f9b17ef3cb334b
JH_Smith <jhsmith7@gmail.com>
2021-10-08 19:42:07 +0200

    Slapping swagger on!

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -0,0 +1,10 @@
+//package za.ac.nwu.ac.domain.persistence;
+//
+//import javax.persistence.*;
+//import java.io.Serializable;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = )
+//public class AccountType {
+//}
